2a3114a99f9cf3cb8d8fd10b54769917c0374055
Dhruv Datt <dhruv.datt@talentica.com>
2017-03-14 14:51:01 +0530

    Adding source index

---
+++ src/index.js
@@ -0,0 +1,14 @@
+var React = require('react');
+var katex = require('katex');
+
+LatexComponent = React.createClass({
+    "render":function(){
+        var string = "\int_{a}^{b} f(x)dx = F(b) - F(a)";
+        var KatexString = katex.renderToString(string);
+        return(
+            <span dangerouslySetInnerHTML={{__html: KatexString}}/>
+        )
+    }
+})
+    
+module.exports = LatexComponent;
