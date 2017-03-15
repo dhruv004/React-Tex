afb6db038f040a4df7cc74e718d0636407156010
Dhruv Datt <dhruv.datt@talentica.com>
2017-03-15 11:39:36 +0530

    added babelrc

--- src/js/index.js
+++ src/js/index.js
@@ -1,16 +1,14 @@
-var React = require('react');
-var katex = require('katex');
+import React,{Component} from 'react';
+import katex from 'katex';
 
-LatexComponent = React.createClass({
-    "render":function(){
+class LatexComponent exntends Component{
+    render(){
         var string = "\int_{a}^{b} f(x)dx = F(b) - F(a)";
         var KatexString = katex.renderToString(string);
         return(
             <span dangerouslySetInnerHTML={{__html: KatexString}}/>
         )
     }
-})
-    
-module.exports = {
-    "katex":LatexComponent
 }
+
+export default LatexComponent
